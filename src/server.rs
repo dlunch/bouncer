b1b99ef07381656a0051f64e16b08ae5c43f6dfe
Inseok Lee <dlunch@gmail.com>
2020-07-05 10:44:54 +0900

    Do not miss accept

--- src/server.rs
+++ src/server.rs
@@ -1,38 +1,60 @@
-
-use async_std::{io, io::BufReader, net::{Ipv4Addr, TcpListener, TcpStream}};
-use futures::{io::AsyncBufReadExt, FutureExt, poll, task::Poll, pin_mut, future::select_all};
+use async_std::{
+    io,
+    io::BufReader,
+    net::{Ipv4Addr, TcpListener, TcpStream},
+    task,
+    task::JoinHandle,
+};
+use futures::{
+    channel::{mpsc, mpsc::UnboundedReceiver},
+    future::select_all,
+    io::AsyncBufReadExt,
+    poll,
+    task::Poll,
+    FutureExt, StreamExt,
+};
 use irc::proto::Message;
 
 #[allow(dead_code)]
 pub struct Server {
-    listener: TcpListener,
-    streams: Vec<TcpStream>,
+    accept_receiver: UnboundedReceiver<TcpStream>,
+    accepter_join_handle: JoinHandle<()>,
+    streams: Vec<(BufReader<TcpStream>, TcpStream)>,
 }
 
 impl Server {
     pub async fn new(port: u16) -> io::Result<Self> {
+        let (sender, accept_receiver) = mpsc::unbounded();
+
         let listener = TcpListener::bind((Ipv4Addr::new(0, 0, 0, 0), port)).await?;
+        let accepter_join_handle = task::spawn(async move {
+            let mut incoming = listener.incoming();
+
+            while let Some(stream) = incoming.next().await {
+                let stream = stream.unwrap();
+                sender.unbounded_send(stream).unwrap();
+            }
+        });
 
         Ok(Self {
-            listener,
+            accept_receiver,
+            accepter_join_handle,
             streams: Vec::new(),
         })
     }
 
     pub async fn next_message(&mut self) -> io::Result<Message> {
-        let accept_future = self.listener.accept();
-        pin_mut!(accept_future);
+        let accept_future = self.accept_receiver.next();
 
         #[allow(unused_variables)]
         if let Poll::Ready(x) = poll!(accept_future) {
-            // TODO accept
+            let stream = x;
         }
 
-        let reader = self.streams.iter().map(|x| {
+        let reader = self.streams.iter_mut().map(|x| {
             async move {
-                let mut reader = BufReader::new(x);
                 let mut result = String::new();
-                reader.read_line(&mut result).await.unwrap();
+                x.0.read_line(&mut result).await.unwrap();
 
                 result
             }
