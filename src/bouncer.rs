f5e46f81702283c805b3c15432c0176102ac3ce5
Inseok Lee <dlunch@gmail.com>
2020-07-11 16:17:59 +0900

    Implement server send

--- src/bouncer.rs
+++ src/bouncer.rs
@@ -6,28 +6,30 @@ use crate::server::Server;
 
 enum Message {
     ServerMessage(irc::proto::Message),
-    ClientMessage(irc::proto::Message)
+    ClientMessage(irc::proto::Message),
 }
 
 pub struct Bouncer {}
 
 impl Bouncer {
     pub async fn run(host: String, port: u16) {
-        let (sender, receiver) = channel(10);
+        let (server_sender, server_receiver) = channel(10);
+        let (client_sender, client_receiver) = channel(10);
 
         let client_join_handle = task::spawn(async move {
             let mut client = Client::new(host, port).await.unwrap();
             let mut client_stream = client.stream().unwrap().fuse();
-            let mut receiver = receiver.fuse();
+            let mut server_receiver = server_receiver.fuse();
 
             loop {
                 let res = select! {
-                    client_message = client_stream.next() => Message::ClientMessage(client_message.unwrap().unwrap()),
-                    server_message = receiver.next() => Message::ServerMessage(server_message.unwrap()),
+                    message = client_stream.next() => Message::ClientMessage(message.unwrap().unwrap()),
+                    message = server_receiver.next() => Message::ServerMessage(message.unwrap()),
                 };
 
-                if let Message::ServerMessage(message) = res {
-                    client.send_message(message).unwrap();
+                match res {
+                    Message::ServerMessage(message) => client.send_message(message).unwrap(),
+                    Message::ClientMessage(message) => client_sender.send(message).await,
                 }
             }
         });
@@ -35,13 +37,17 @@ impl Bouncer {
         let server_join_handle = task::spawn(async move {
             let mut server = Server::new(6667).await.unwrap();
             let mut server_stream = server.stream().fuse();
+            let mut client_receiver = client_receiver.fuse();
+
             loop {
                 let res = select! {
-                    server_message = server_stream.next() => Message::ServerMessage(server_message.unwrap())
+                    message = server_stream.next() => Message::ServerMessage(message.unwrap()),
+                    message = client_receiver.next() => Message::ClientMessage(message.unwrap()),
                 };
 
-                if let Message::ServerMessage(message) = res {
-                    sender.send(message).await;
+                match res {
+                    Message::ServerMessage(message) => server_sender.send(message).await,
+                    Message::ClientMessage(message) => server.send_message(message).await.unwrap(),
                 }
             }
         });
--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,13 @@
+use std::{collections::HashMap, sync::Arc};
+
 use async_std::{
     net::{Ipv4Addr, TcpListener, TcpStream},
-    sync::{channel, Receiver, Sender},
+    sync::{channel, Mutex, Receiver, Sender},
     task,
 };
 use futures::{
     io,
-    io::{AsyncBufReadExt, BufReader},
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     stream::Stream,
     StreamExt,
 };
@@ -13,8 +15,40 @@ use irc::proto::Message;
 
 type ReadMessage = (String, TcpStream);
 
+struct Streams {
+    data: HashMap<u32, TcpStream>,
+    index: u32,
+}
+
+impl Streams {
+    pub fn new() -> Self {
+        Self {
+            data: HashMap::new(),
+            index: 0,
+        }
+    }
+
+    pub fn insert(&mut self, stream: &TcpStream) -> u32 {
+        let index = self.index;
+        self.index += 1;
+
+        self.data.insert(index, stream.clone());
+
+        index
+    }
+
+    pub fn remove(&mut self, index: u32) {
+        self.data.remove(&index);
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut TcpStream> {
+        self.data.iter_mut().map(|x| x.1)
+    }
+}
+
 pub struct Server {
     receiver: Receiver<ReadMessage>,
+    streams: Arc<Mutex<Streams>>,
 }
 
 impl Server {
@@ -22,38 +56,57 @@ impl Server {
         let listener = TcpListener::bind((Ipv4Addr::new(0, 0, 0, 0), port)).await?;
 
         let (sender, receiver) = channel(10);
+        let streams = Arc::new(Mutex::new(Streams::new()));
+
+        let streams2 = streams.clone();
         task::spawn(async move {
-            Self::accept_loop(listener, sender).await.unwrap();
+            Self::accept_loop(listener, sender, streams2).await.unwrap();
         });
 
-        Ok(Self { receiver })
+        Ok(Self { receiver, streams })
     }
 
-    async fn accept_loop(listener: TcpListener, sender: Sender<ReadMessage>) -> io::Result<()> {
+    async fn accept_loop(listener: TcpListener, sender: Sender<ReadMessage>, streams: Arc<Mutex<Streams>>) -> io::Result<()> {
         let mut incoming = listener.incoming();
 
         while let Some(stream) = incoming.next().await {
             let stream = stream?;
             let sender = sender.clone();
+
+            let streams2 = streams.clone();
             task::spawn(async move {
-                Self::read_loop(stream, sender).await.unwrap();
+                Self::read_loop(stream, sender, streams2).await.unwrap();
             });
         }
 
         Ok(())
     }
 
-    async fn read_loop(stream: TcpStream, sender: Sender<ReadMessage>) -> io::Result<()> {
+    async fn read_loop(stream: TcpStream, sender: Sender<ReadMessage>, streams: Arc<Mutex<Streams>>) -> io::Result<()> {
+        let index = streams.lock().await.insert(&stream);
+
         let reader = BufReader::new(&stream);
         let mut lines = reader.lines();
         while let Some(line) = lines.next().await {
             sender.send((line?, stream.clone())).await;
         }
 
+        streams.lock().await.remove(index);
+
         Ok(())
     }
 
     pub fn stream(&mut self) -> impl Stream<Item = Message> {
         self.receiver.clone().map(|x| x.0.parse::<Message>().unwrap())
+    }
+
+    pub async fn send_message(&self, message: Message) -> io::Result<()> {
+        let mut streams = self.streams.lock().await;
+
+        for stream in streams.iter_mut() {
+            stream.write(message.to_string().as_bytes()).await?;
+        }
+
+        Ok(())
     }
 }
