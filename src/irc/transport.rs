53ef9af2672f98e69a76d282cf9124f113f377df
Inseok Lee <dlunch@gmail.com>
2020-08-09 16:35:55 +0900

    Use common transport

--- src/irc/transport.rs
+++ src/irc/transport.rs
@@ -2,6 +2,7 @@ use async_std::{io::Result, net::TcpStream};
 use futures::{io::BufReader, AsyncBufReadExt, AsyncWriteExt, Stream, StreamExt};
 use irc_proto::Message;
 
+#[derive(Clone)]
 pub struct Transport {
     stream: TcpStream,
 }
@@ -11,17 +12,17 @@ impl Transport {
         Self { stream }
     }
 
-    pub fn stream(&self) -> Result<impl Stream<Item = Result<Message>>> {
+    pub fn stream(&self) -> impl Stream<Item = Result<Message>> {
         let reader = BufReader::new(self.stream.clone());
 
-        Ok(reader.lines().map(move |x| {
+        reader.lines().map(move |x| {
             let message = x?.parse::<Message>().unwrap();
 
             Ok(message)
-        }))
+        })
     }
 
-    pub async fn send_message(&self, message: Message) -> Result<()> {
+    pub async fn send_message(&self, message: &Message) -> Result<()> {
         let mut stream = self.stream.clone();
         stream.write(message.to_string().as_bytes()).await?;
 
