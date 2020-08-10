6e276030cbb8a1197a2745e387006058ece910ca
Inseok Lee <dlunch@gmail.com>
2020-08-10 17:19:22 +0900

    Rename

--- src/bouncer.rs
+++ src/bouncer.rs
@@ -16,27 +16,30 @@ enum BouncerMessage {
 }
 
 pub struct Bouncer {
-    sink_sender: Sender<Message>,
-    source_sender: Sender<Message>,
+    sink_to_source: Sender<Message>,
+    source_to_sink: Sender<Message>,
 }
 
 impl Bouncer {
     pub async fn run(host: String, port: u16) {
-        let (sink_sender, sink_receiver) = channel(10);
-        let (source_sender, source_receiver) = channel(10);
+        let (sink_to_source, sink_from_source) = channel(10);
+        let (source_to_sink, source_from_sink) = channel(10);
 
-        let bouncer = Arc::new(Self { sink_sender, source_sender });
+        let bouncer = Arc::new(Self {
+            sink_to_source,
+            source_to_sink,
+        });
 
         let bouncer_clone = bouncer.clone();
         let source_join_handle = task::spawn(async move {
             let source = Client::new(host, port).await.unwrap();
             let mut source_stream = source.stream().fuse();
-            let mut sink_receiver = sink_receiver.fuse();
+            let mut sink_from_source = sink_from_source.fuse();
 
             loop {
                 let res = select! {
                     message = source_stream.next() => BouncerMessage::SourceMessage(message.unwrap()),
-                    message = sink_receiver.next() => BouncerMessage::SinkMessage(message.unwrap()),
+                    message = sink_from_source.next() => BouncerMessage::SinkMessage(message.unwrap()),
                 };
 
                 match res {
@@ -50,12 +53,12 @@ impl Bouncer {
         let sink_join_handle = task::spawn(async move {
             let sink = Server::new(6667).await.unwrap();
             let mut sink_stream = sink.stream().fuse();
-            let mut source_receiver = source_receiver.fuse();
+            let mut source_from_sink = source_from_sink.fuse();
 
             loop {
                 let res = select! {
                     message = sink_stream.next() => BouncerMessage::SinkMessage(message.unwrap()),
-                    message = source_receiver.next() => BouncerMessage::SourceMessage(message.unwrap()),
+                    message = source_from_sink.next() => BouncerMessage::SourceMessage(message.unwrap()),
                 };
 
                 match res {
@@ -69,13 +72,13 @@ impl Bouncer {
     }
 
     async fn handle_source_message(&self, message: Message) {
-        self.source_sender.send(message).await
+        self.source_to_sink.send(message).await
     }
 
     async fn handle_sink_message(&self, message: Message) {
         if let Command::USER(_, _, _) = &message.command {
             return;
         }
-        self.sink_sender.send(message).await
+        self.sink_to_source.send(message).await
     }
 }
