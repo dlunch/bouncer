6832abe3fe6b1bd8db52b7ffa1c23290bfa89722
Inseok Lee <dlunch@gmail.com>
2020-08-30 10:06:02 +0900

    Implement message parse

--- src/irc/message.rs
+++ src/irc/message.rs
@@ -1,3 +1,5 @@
+use std::iter;
+
 pub struct Message {
     pub prefix: Option<String>,
     pub command: String,
@@ -13,12 +15,28 @@ impl Message {
         Self { prefix, command, args }
     }
 
-    pub fn from_raw(_: String) -> Self {
-        Self {
-            prefix: None,
-            command: "".to_owned(),
-            args: vec![],
+    pub fn from_raw(raw: String) -> Self {
+        let mut split = raw.split(' ').peekable();
+
+        let prefix = if split.peek().unwrap().starts_with(':') {
+            Some(split.next().unwrap()[1..].into())
+        } else {
+            None
+        };
+
+        let command = split.next().unwrap().into();
+
+        let mut args = Vec::<String>::with_capacity(split.size_hint().0);
+        while let Some(item) = split.next() {
+            if item.starts_with(':') {
+                args.push(iter::once(&item[1..]).chain(split).collect::<Vec<_>>().join(" "));
+                break;
+            } else {
+                args.push(item.into())
+            }
         }
+
+        Self { prefix, command, args }
     }
 
     pub fn raw(&self) -> String {
@@ -29,5 +47,40 @@ impl Message {
 impl std::fmt::Display for Message {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.raw())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_simple() {
+        let message = Message::from_raw("NICK test".into());
+
+        assert_eq!(message.command, "NICK");
+        assert_eq!(message.args.len(), 1);
+        assert_eq!(message.args[0], "test");
+    }
+
+    #[test]
+    fn test_parse_trailing() {
+        let message = Message::from_raw("PRIVMSG #test :test test".into());
+
+        assert_eq!(message.command, "PRIVMSG");
+        assert_eq!(message.args.len(), 2);
+        assert_eq!(message.args[0], "#test");
+        assert_eq!(message.args[1], "test test");
+    }
+
+    #[test]
+    fn test_parse_prefix() {
+        let message = Message::from_raw(":test@test PRIVMSG #test :test test".into());
+
+        assert_eq!(message.prefix, Some("test@test".into()));
+        assert_eq!(message.command, "PRIVMSG");
+        assert_eq!(message.args.len(), 2);
+        assert_eq!(message.args[0], "#test");
+        assert_eq!(message.args[1], "test test");
     }
 }
