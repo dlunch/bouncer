068157af01be78c8b8e57ef668aa70474ef1c9c7
Inseok Lee <dlunch@gmail.com>
2020-09-05 10:21:35 +0900

    More cleanup

--- src/irc/server.rs
+++ src/irc/server.rs
@@ -1,34 +1,134 @@
-mod server_impl;
+use std::{collections::HashMap, sync::Arc};
 
-use std::sync::Arc;
+use async_std::{
+    io::Result,
+    net::{Ipv4Addr, TcpListener},
+    sync::{channel, Mutex, Receiver, Sender},
+    task,
+};
+use futures::{FutureExt, Stream, StreamExt};
+use log::debug;
 
-use async_std::io::Result;
-use futures::{Stream, StreamExt};
+use super::{transport::Transport, Message};
 
-use super::Message;
+struct Transports {
+    data: HashMap<u32, Transport>,
+    index: u32,
+}
+
+impl Transports {
+    pub fn new() -> Self {
+        Self {
+            data: HashMap::new(),
+            index: 0,
+        }
+    }
+
+    pub fn insert(&mut self, transport: &Transport) -> u32 {
+        let index = self.index;
+        self.index += 1;
+
+        self.data.insert(index, transport.clone());
+
+        index
+    }
+
+    pub fn remove(&mut self, index: u32) {
+        self.data.remove(&index);
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut Transport> {
+        self.data.iter_mut().map(|x| x.1)
+    }
+}
 
 pub struct Server {
-    server: Arc<server_impl::ServerImpl>,
+    receiver: Receiver<(Message, Transport)>,
+    streams: Arc<Mutex<Transports>>,
 }
 
 impl Server {
     pub async fn new(port: u16) -> Result<Self> {
-        let server = Arc::new(server_impl::ServerImpl::new(port).await?);
+        let listener = TcpListener::bind((Ipv4Addr::new(0, 0, 0, 0), port)).await?;
 
-        Ok(Self { server })
+        let (sender, receiver) = channel(10);
+        let streams = Arc::new(Mutex::new(Transports::new()));
+
+        let result = Self { receiver, streams };
+
+        let streams = result.streams.clone();
+        task::spawn(async {
+            Self::accept_loop(listener, sender, streams).await.unwrap();
+        });
+
+        Ok(result)
     }
 
-    pub fn stream(&self) -> impl Stream<Item = Message> {
-        let server = self.server.clone();
+    async fn accept_loop(listener: TcpListener, sender: Sender<(Message, Transport)>, transports: Arc<Mutex<Transports>>) -> Result<()> {
+        let mut incoming = listener.incoming();
 
-        self.server.stream().map(move |(message, sender)| {
-            server.handle_message(sender, &message).unwrap();
+        while let Some(stream) = incoming.next().await {
+            let transport = Transport::new(stream?);
+            let sender = sender.clone();
 
-            message
+            let transports = transports.clone();
+            task::spawn(async move {
+                Self::read_loop(transport, sender, transports).await.unwrap();
+            });
+        }
+
+        Ok(())
+    }
+
+    async fn read_loop(transport: Transport, sender: Sender<(Message, Transport)>, transports: Arc<Mutex<Transports>>) -> Result<()> {
+        let index = transports.lock().await.insert(&transport);
+
+        let mut stream = transport.stream();
+        while let Some(message) = stream.next().await {
+            sender.send((message, transport.clone())).await;
+        }
+
+        transports.lock().await.remove(index);
+
+        Ok(())
+    }
+
+    pub fn stream<'a>(&'a self) -> impl Stream<Item = Message> + 'a {
+        self.receiver.clone().then(move |(message, sender)| {
+            async move {
+                self.handle_message(&sender, &message).await.unwrap();
+
+                message
+            }
+            .boxed()
         })
     }
 
     pub async fn broadcast(&self, message: Message) -> Result<()> {
-        self.server.broadcast(message).await
+        debug!("Broadcast: {}", message);
+
+        let mut streams = self.streams.lock().await;
+
+        for stream in streams.iter_mut() {
+            stream.send_message(&message).await?;
+        }
+
+        Ok(())
+    }
+
+    async fn send_response(&self, receiver: &Transport, message: Message) -> Result<()> {
+        receiver.send_message(&message).await
+    }
+
+    pub async fn handle_message(&self, sender: &Transport, message: &Message) -> Result<()> {
+        debug!("From Client: {}", message);
+
+        if message.command == "PING" {
+            let response = Message::new(None, "PONG", vec![message.args[0].as_ref()]);
+
+            self.send_response(&sender, response).await?;
+        }
+
+        Ok(())
     }
 }
