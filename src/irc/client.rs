d99ec8abe0bf00e76a57c6a01b1a7683c01a6331
Inseok Lee <dlunch@gmail.com>
2020-09-05 10:21:35 +0900

    More cleanup

--- src/irc/client.rs
+++ src/irc/client.rs
@@ -1,33 +1,38 @@
-mod client_impl;
-
-use std::sync::Arc;
-
-use async_std::io::Result;
+use async_std::{
+    io::Result,
+    net::{TcpStream, ToSocketAddrs},
+};
 use futures::{FutureExt, Stream, StreamExt};
+use log::debug;
 
-use super::Message;
+use super::{transport::Transport, Message};
 
 pub struct Client {
-    client: Arc<client_impl::ClientImpl>,
+    transport: Transport,
 }
 
 impl Client {
     pub async fn new(host: String, port: u16) -> Result<Self> {
-        let client = Arc::new(client_impl::ClientImpl::new(host, port).await?);
+        let addr = (host.as_ref(), port).to_socket_addrs().await?.next().unwrap();
+        let stream = TcpStream::connect(addr).await?;
 
-        client.send_message(Message::new(None, "USER", vec!["test", "0", "*", "test"])).await?;
-        client.send_message(Message::new(None, "NICK", vec!["testtest"])).await?;
+        let transport = Transport::new(stream);
 
-        Ok(Self { client })
+        transport.send_message(&Message::new(None, "USER", vec!["test", "0", "test"])).await?;
+        transport.send_message(&Message::new(None, "NICK", vec!["testtest"])).await?;
+
+        let result = Self { transport };
+
+        result.send_message(Message::new(None, "USER", vec!["test", "0", "*", "test"])).await?;
+        result.send_message(Message::new(None, "NICK", vec!["testtest"])).await?;
+
+        Ok(result)
     }
 
-    pub fn stream(&self) -> impl Stream<Item = Message> {
-        let client = self.client.clone();
-
-        self.client.stream().then(move |message: Message| {
-            let client = client.clone();
+    pub fn stream<'a>(&'a self) -> impl Stream<Item = Message> + 'a {
+        self.transport.stream().then(move |message: Message| {
             async move {
-                client.handle_message(&message).await.unwrap();
+                self.handle_message(&message).await.unwrap();
 
                 message
             }
@@ -36,6 +41,33 @@ impl Client {
     }
 
     pub async fn send_message(&self, message: Message) -> Result<()> {
-        self.client.send_message(message).await
+        debug!("To Origin: {}", message);
+
+        self.transport.send_message(&message).await?;
+
+        Ok(())
+    }
+
+    fn on_connected(&self) -> Result<()> {
+        Ok(())
+    }
+
+    pub async fn handle_message(&self, message: &Message) -> Result<()> {
+        debug!("From Origin: {}", message);
+
+        match message.command.as_ref() {
+            "PING" => {
+                let response = Message::new(None, "PONG", vec![message.args[0].as_ref()]);
+
+                self.send_message(response).await?;
+            }
+            "376" | "422" => {
+                // RPL_ENDOFMOTD | ERR_NOMOTD
+                self.on_connected()?
+            }
+            _ => {}
+        }
+
+        Ok(())
     }
 }
